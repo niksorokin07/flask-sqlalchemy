d6aa2632fd13da32033337958b626e3ba4045265
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2023-03-01 19:07:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, redirect
+import datetime
+from flask_login import LoginManager, login_user, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+from data import db_session
+from data.db_session import SqlAlchemyBase
+from data.users import User
+import sqlalchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+db_session.global_init("db/blogs.db")
+dbs = db_session.create_session()
+
+user = User()
+user.surname = "Scott"
+user.name = "Ridley"
+user.age = 21
+user.position = "captain"
+user.speciality = "research engineer"
+user.address = "module_1"
+user.email = "scott_chief@mars.org"
+user.password_hash = "cap"
+user.set_password("cap")
+dbs.add(user)
+dbs.commit()
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "Ohae godzaimasu!"
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html', message="Неправильный логин или пароль", form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    app.run(port=8080, host='127.0.0.1')
