8ddb0aabff60e047bdc635d034982c6c7435cb3a
niksorokin07 <117514209+niksorokin07@users.noreply.github.com>
2023-03-02 08:57:30 +0300

    Номер 2

--- data/db_session.py
+++ data/db_session.py
@@ -19,7 +19,7 @@ def global_init(db_file):
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
     print(f"Подключение к базе данных по адресу {conn_str}")
-    from data import users
+    from . import __all_models
     engine = sqla.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect
 import datetime
-from flask_login import LoginManager, login_user, UserMixin
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, SubmitField, EmailField
+from wtforms import PasswordField, BooleanField, SubmitField, EmailField, StringField, IntegerField
 from wtforms.validators import DataRequired
 from data import db_session
 from data.db_session import SqlAlchemyBase
@@ -15,21 +15,6 @@ app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-db_session.global_init("db/blogs.db")
-dbs = db_session.create_session()
-
-user = User()
-user.surname = "Scott"
-user.name = "Ridley"
-user.age = 21
-user.position = "captain"
-user.speciality = "research engineer"
-user.address = "module_1"
-user.email = "scott_chief@mars.org"
-user.password_hash = "cap"
-user.set_password("cap")
-dbs.add(user)
-dbs.commit()
 
 
 class LoginForm(FlaskForm):
@@ -37,6 +22,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age')
+    pos = StringField('Position')
+    spec = StringField('Speciality')
+    adrs = StringField("Address")
+    submit = SubmitField('Register')
 
 
 @login_manager.user_loader
@@ -62,6 +60,41 @@ def login():
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Register form',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Register form',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.pos.data,
+            address=form.adrs.data,
+            speciality=form.spec.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('register.html', title='Register form', form=form)
 
 
 if __name__ == '__main__':
